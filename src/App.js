0f6cdce59edd64a72368038f389c0702044339d5
Tropicwarrior <joeyper03@gmail.com>
2021-11-12 11:14:29 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+export default function App() {
+  import React from 'react';
+ 
+  const [todos, setTodos] = react.useState([
+    { id: 1, text: "Wash Dishes", done: false },
+    { id: 2, text: "Do Laundry", done: false },
+    { id: 3, text: "Take Shower", done: false }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} />
+      <AddTodo setTodos={setTodos} />
     </div>
+  );
+
+
+function TodoList({ todos }) {
+  console.log(todos);
+  return (
+    <ul>
+      {" "}
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
   );
 }
 
-export default App;
+function AddTodo({ setTodos }) {
+  const inputRef = react.useRef();
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: 4,
+      text,
+      done: false
+    };
+    setTodos((prevTodos) => {
+      return prevTodos.concat(todo);
+    });
+
+  }
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add todo" ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Finals Grade Calculator
+        </h1>
+        <p>
+          This Program will allow you to find out what grade you need on your final to get the grade you want in the class
+          you can put you're curret grade in the first imput section and youre desired grade in the seccond.
+          
+        </p>
+  
+      </header>
+    </div>
+  )
+}
+}
+
+
