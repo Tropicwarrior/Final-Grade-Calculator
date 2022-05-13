179c09c078ef2156d053c4a8c4da27975f56bd7c
Tropicwarrior <joeyper03@gmail.com>
2022-05-13 11:27:20 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,76 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
+const App = () => {
+  const [dGrade, setDgrade] = useState([]);
+  const addDgrade = (e) => {
+    e.preventDefault();
+    setDgrade([dGrade]);
+    e.target.dGrade = "";
+  };
 
+  useEffect(() => {
+    const json = JSON.stringify(dGrade);
+    localStorage.setItem("dGrade", json);
+  }, [dGrade]);
 
-export default function App() {
-  import React from 'react';
- 
-  const [todos, setTodos] = react.useState([
-    { id: 1, text: "Wash Dishes", done: false },
-    { id: 2, text: "Do Laundry", done: false },
-    { id: 3, text: "Take Shower", done: false }
-  ]);
+  const [grade, setGrade] = useState([]);
+  const addGrade = (e) => {
+    e.preventDefault();
+    setGrade([grade]);
+    e.target.grade = "";
+  };
+  useEffect(() => {
+    const json = JSON.stringify(grade);
+    localStorage.setItem("grade", json);
+  }, [grade]);
 
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <TodoList todos={todos} />
-      <AddTodo setTodos={setTodos} />
-    </div>
-  );
-
-
-function TodoList({ todos }) {
-  console.log(todos);
-  return (
-    <ul>
-      {" "}
-      {todos.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
-      ))}
-    </ul>
-  );
-}
-
-function AddTodo({ setTodos }) {
-  const inputRef = react.useRef();
-
-  function handleAddTodo(event) {
-    event.preventDefault();
-    const text = event.target.elements.addTodo.value;
-    const todo = {
-      id: 4,
-      text,
-      done: false
-    };
-    setTodos((prevTodos) => {
-      return prevTodos.concat(todo);
-    });
-
-  }
-  return (
-    <form onSubmit={handleAddTodo}>
-      <input name="addTodo" placeholder="Add todo" ref={inputRef} />
-      <button type="submit">Submit</button>
-    </form>
-  );
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Finals Grade Calculator
-        </h1>
-        <p>
-          This Program will allow you to find out what grade you need on your final to get the grade you want in the class
-          you can put you're curret grade in the first imput section and youre desired grade in the seccond.
-          
-        </p>
-  
-      </header>
+      <h1>Finals Grade Calculator</h1>
+      <p>
+        This program will allow you to find out what grade you need on your
+        final to get the grade you want in the class you can put you're curent
+        grade in the first input section and youre desired grade in the seccond.
+      </p>
+      <h3>Current Grade</h3>
+      <form onSubmit={addGrade}>
+        <input type="text" name="note" />
+        <input type="Submit" />
+      </form>
+      {grade.map((grade) => (
+        <div key={grade.id}> {grade.text} </div>
+      ))}
+      <h3>Desired Grade</h3>
+      <form onSubmit={addDgrade}>
+        <input type="text" name="grade" />
+        <input type="Submit" />
+      </form>
+      {grade.map((grade) => (
+        <div key={grade.id}> {grade.text} </div>
+      ))}
     </div>
-  )
-}
-}
+  );
+};
 
-
+export default App;
